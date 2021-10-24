321c2e3ef0144d38fb2cd62d87dfb57efb035d84
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-10-24 12:33:18 +0530

    Added: Http services

---
+++ src/app/services/HttpService/http.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  PostService(url : string, data:any = null, isHeaderRequired: any = false, headers: any = null) {
+    return this.http.post(url, data, isHeaderRequired && headers)
+  }
+
+}
