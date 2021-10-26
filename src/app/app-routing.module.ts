8e6378da6c5878e542c28169aefeda510249873d
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-10-26 11:26:33 +0530

    Added: routing for get all books component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { HomeComponent } from './components/home/home.component';
+import { GetAllBooksComponent } from './components/get-all-books/get-all-books.component';
 
 const routes: Routes = [
   { path: 'signup', component: LoginComponent },
-  { path: 'forgotPassword', component: ForgotPasswordComponent }
+  { path: 'forgotPassword', component: ForgotPasswordComponent },
+  {
+    path: 'home', component: HomeComponent,
+    children: [
+      { path: 'allbooks', component: GetAllBooksComponent },
+    ]
+  },
 ];
 
 @NgModule({
