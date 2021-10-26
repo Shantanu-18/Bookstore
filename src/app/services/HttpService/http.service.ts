45761d5d82757230746ee3e40ec298b98572601b
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-10-26 11:30:49 +0530

    Added: http get service

--- src/app/services/HttpService/http.service.ts
+++ src/app/services/HttpService/http.service.ts
@@ -8,8 +8,12 @@ export class HttpService {
 
   constructor(private http: HttpClient) { }
 
-  PostService(url : string, data:any = null, isHeaderRequired: any = false, headers: any = null) {
+  PostService(url: string, data: any = null, isHeaderRequired: any = false, headers: any = null) {
     return this.http.post(url, data, isHeaderRequired && headers)
+  }
+
+  GetService(url: string,isHeaderRequired: any = false, headers: any = null) {
+    return this.http.get(url,isHeaderRequired && headers)
   }
 
 }
