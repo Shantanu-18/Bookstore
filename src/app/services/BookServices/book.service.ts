209775683af075e943c96939a5c0d8f699ac95e5
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-10-28 23:31:17 +0530

    Added: required book services

--- src/app/services/BookServices/book.service.ts
+++ src/app/services/BookServices/book.service.ts
@@ -7,9 +7,9 @@ import { HttpService } from '../HttpService/http.service';
   providedIn: 'root'
 })
 export class BookService {
-  BaseUrl = environment.BaseUrl
+  baseURL = environment.BaseUrl
 
-  constructor(private httpService: HttpService) { }
+  constructor(private http: HttpService) { }
 
   gelAllBookService() {
     let headers = {
@@ -17,6 +17,91 @@ export class BookService {
         'content-Type': 'application/json'
       })
     }
-    return this.httpService.PostService(this.BaseUrl + '/bookstore_user/get/book', false, headers);
+    return this.http.GetService(this.baseURL + '/bookstore_user/get/book', false, headers);
+  }
+
+  getwishlist() {
+    let headers = {
+      headers: new HttpHeaders({
+        'content-Type': 'application/json'
+      })
+    }
+
+    return this.http.GetService(this.baseURL + "/bookstore_user/get_wishlist_items", true, headers)
+  }
+
+  addcartitem(productID: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'content-Type': 'application/json'
+      })
+    }
+    return this.http.PostService(this.baseURL + "/bookstore_user/add_cart_item/" + productID, null, true, headers);
+  }
+
+  updateitemcount(productID: any, payload: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'content-Type': 'application/json'
+      })
+    }
+    return this.http.PutService(this.baseURL + "/bookstore_user/cart_item_quantity/" + productID, payload, true, headers);
+  }
+
+  getCartItems() {
+    let headers = {
+      headers: new HttpHeaders({
+        'content-Type': 'application/json'
+      })
+    }
+
+    return this.http.GetService(this.baseURL + "/bookstore_user/get_cart_items", true, headers)
+  }
+
+  removecartitem(productID: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'content-Type': 'application/json'
+      })
+    }
+    return this.http.DeleteService(this.baseURL + "/bookstore_user/remove_cart_item/" + productID, null, true, headers);
+  }
+
+  orderplace(payload: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'content-Type': 'application/json'
+      })
+    }
+    return this.http.PostService(this.baseURL + "/bookstore_user/add/order", payload, true, headers);
+  }
+
+
+
+  addwishlist(productID: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'content-Type': 'application/json'
+      })
+    }
+    return this.http.PostService(this.baseURL + "/bookstore_user/add_wish_list/" + productID, null, true, headers);
+  }
+
+  removewishlistitem(productID: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'content-Type': 'application/json'
+      })
+    }
+    return this.http.DeleteService(this.baseURL + "/bookstore_user/remove_wishlist_item/" + productID, null, true, headers);
+  }
+
+  updateaddress(payload: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'content-Type': 'application/json'
+      })
+    }
+    return this.http.PutService(this.baseURL + "/bookstore_user/edit_user", payload, true, headers);
   }
 }
