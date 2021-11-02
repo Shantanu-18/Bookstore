45b19deebbbf127dc1837960e9b651e97574ed20
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-11-02 23:19:59 +0530

    Added: method to get cart items and badge to show number of cart items

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BookService } from 'src/app/services/BookServices/book.service';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +10,38 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
   fullName = localStorage.getItem('fullName');
   name = this.fullName?.split(" ")[0];
+  cartNumber: any;
 
-  constructor(private router:Router) { }
+  constructor(private router: Router, private bookService: BookService) { }
 
   ngOnInit(): void {
+    this.getCardNumber();
   }
 
   homeButton() {
     this.router.navigateByUrl('/home/allbooks')
+  }
+
+  profileButton() {
+    this.router.navigateByUrl('/home/profile')
+  }
+
+  onCart() {
+    this.bookService.getCartItems().subscribe((res: any) => {
+      console.log(res.result);
+    },
+      (error) => console.log(error)
+    )
+
+    this.router.navigateByUrl('/home/cart')
+  }
+
+  getCardNumber() {
+    this.bookService.getCartItems().subscribe((res: any) => {
+      this.cartNumber = res.result.length;
+      console.log("length", res.result.length);
+    },
+      (error) => console.log(error)
+    )
   }
 }
